6702988f163a864641b3197a056e0a9da5213f42
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2023-11-22 16:43:55 +0200

    Osa 4 apufunktioita ja yksikkötestejä 3

--- osa4/apufunktioita_ja_yksikkotesteja/tests/bloglist.test.js
+++ osa4/apufunktioita_ja_yksikkotesteja/tests/bloglist.test.js
@@ -1,31 +1,31 @@
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-  const blogs = []
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
 })
 
 describe('total likes', () => {
     const listWithOneBlog = [
       {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Testing 1',
-        author: 'Mr Tester',
-        url: 'test_url',
-        likes: 5,
-        __v: 0
-      }
-    ]
-    const listWithMoreBlogs = [
-        {
           _id: '5a422aa71b54a676234d17f8',
           title: 'Testing 1',
           author: 'Mr Tester',
           url: 'test_url',
           likes: 5,
           __v: 0
+      }
+    ]
+    const listWithMoreBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Testing 1',
+            author: 'Mr Tester',
+            url: 'test_url',
+            likes: 5,
+            __v: 0
         },
         {
             _id: '558b3292f3572652a58525c7',
@@ -41,15 +41,70 @@ describe('total likes', () => {
         blogs = []
         const result = listHelper.totalLikes(blogs)
         expect(result).toBe(0)
-      })
+    })
 
     test('when list has only one blog equals the likes of that', () => {
-      const result = listHelper.totalLikes(listWithOneBlog)
-      expect(result).toBe(5)
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
     })
 
     test('of a bigger list is calculated right', () => {
         const result = listHelper.totalLikes(listWithMoreBlogs)
         expect(result).toBe(11)
-      })
-  })
+    })
+})
+
+describe('favorite blog', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Testing 1',
+            author: 'Mr Tester',
+            url: 'test_url',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    const listWithMoreBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Testing 1',
+            author: 'Mr Tester',
+            url: 'test_url',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '558b3292f3572652a58525c7',
+            title: 'Something is out there',
+            author: 'Fear',
+            url: 'testtesttest',
+            likes: 6,
+            __v: 0
+        },
+        {
+            _id: '538b3d92f3572852a58a25c7',
+            title: 'I dont know what im doing',
+            author: 'Lost',
+            url: 'testtesttest',
+            likes: 6,
+            __v: 0
+        },
+    ]
+
+    test('of empty list is empty object', () => {
+        blogs = []
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual({})
+    })
+
+    test('when list has only one blog is the favorite blog', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual(listWithOneBlog[0])
+    })
+
+    test('of a bigger list is the one with most likes', () => {
+        const result = listHelper.favoriteBlog(listWithMoreBlogs)
+        expect(result).toEqual(listWithMoreBlogs[1])
+    })
+})
--- osa4/apufunktioita_ja_yksikkotesteja/utils/list_helper.js
+++ osa4/apufunktioita_ja_yksikkotesteja/utils/list_helper.js
@@ -10,8 +10,21 @@ const totalLikes = (blogs) => {
     });
     return total
 }
-  
+
+const favoriteBlog = (blogs) => {
+    favBlog = {}
+    blogs.forEach(blog => {
+        if (!favBlog.likes)
+            favBlog = blog
+        else 
+            if (favBlog.likes < blog.likes) 
+            favBlog = blog
+    });
+    return favBlog
+}
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }

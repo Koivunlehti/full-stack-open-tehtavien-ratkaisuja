8b43da03513ed30049abfc8f232da11d131f93d8
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2023-11-22 17:05:47 +0200

    Osa 4 apufunktioita ja yksikkötestejä 5

--- osa4/apufunktioita_ja_yksikkotesteja/tests/bloglist.test.js
+++ osa4/apufunktioita_ja_yksikkotesteja/tests/bloglist.test.js
@@ -176,3 +176,71 @@ describe('author with most blogs', () => {
         expect(result).toEqual({author:"Tester 7", blogs:3})
     })
 })
+
+describe('author with most likes', () => {
+    const blogs = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: 'Testing 1',
+            author: 'Mr Tester',
+            url: 'test_url',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: 'Something is out there',
+            author: 'Fear',
+            url: 'testtesttest',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: 'What if this is not working?',
+            author: 'Fear',
+            url: 'testtesttest',
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422b891b54a676234d17fa",
+            title: 'Im testing this',
+            author: 'Tester 7',
+            url: 'testtesttest',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "Im still testing this",
+            author: "Tester 7",
+            url: 'testtesttest',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Thousand tests later",
+            author: "Tester 7",
+            url: 'testtesttest',
+            likes: 2,
+            __v: 0
+        }  
+    ]
+
+    test('is empty object when the list is empty', () => {
+        const result = listHelper.mostLikes([])
+        expect(result).toEqual({})
+    })
+
+    test('is the only author on the list when list has only one blog on it', () => {
+        const result = listHelper.mostLikes([blogs[0]])
+        expect(result).toEqual({author:"Mr Tester", likes:7})
+    })
+
+    test('is the one with highest total like count when bigger list of blogs', () => {
+        const result = listHelper.mostLikes(blogs)
+        expect(result).toEqual({author:"Fear", likes:17})
+    })
+})
--- osa4/apufunktioita_ja_yksikkotesteja/utils/list_helper.js
+++ osa4/apufunktioita_ja_yksikkotesteja/utils/list_helper.js
@@ -59,9 +59,46 @@ const mostBlogs = (blogs) => {
     return mostBlogs
 }
 
+const mostLikes = (blogs) => {
+    const blogLikes = []
+    // Käydään läpi kaikki blogit ja kerätään pelkät authorit ja niiden esiintymiskerrat uuteen taulukkoon
+    blogs.forEach(blog => {
+        // Jos blogLikes on tyhjä, lisätään sinne tämän kierroksen author ja merkataan tykkäysten määrä
+        if (blogLikes.length === 0) {     
+            blogLikes.push({author:blog.author, likes:blog.likes})
+        }
+        else {
+            let found = false
+            // Käydään läpi blogLikesiin tähän mennessä lisätyt authorit
+            blogLikes.forEach(author => {
+                // Jos author löytyy jo blogLikes taulukosta, lisätään authorin like määrää tämän hetkisen blogin like määrällä ja merkataan että author löytyi.
+                if (blog.author === author.author){
+                    author.likes += blog.likes
+                    found = true
+                }
+            })
+            // Jos authoria ei löytynyt blogLikes taulukosta tällä kierroksella, lisätään uusi author blogLikes taulukkoon ja merkataan tykkäysten määrä
+            if (found === false){      
+                blogLikes.push({author:blog.author, likes:blog.likes})
+            }
+        }
+    });
+    // Käydään läpi yllä kerätyt authorit ja selvitetään kenellä on suurin like määrä.
+    let mostLikes = {}
+    blogLikes.forEach(author => {
+        if (!mostLikes.likes)
+            mostLikes = author
+        else 
+            if (mostLikes.likes < author.likes) 
+            mostLikes = author
+    })
+    return mostLikes
+}
+
 module.exports = {
 dummy,
 totalLikes,
 favoriteBlog,
 mostBlogs,
+mostLikes
 }

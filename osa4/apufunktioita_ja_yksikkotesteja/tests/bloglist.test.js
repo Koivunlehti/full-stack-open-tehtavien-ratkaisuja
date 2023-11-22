d951a0b6f252871562aaebfc6f6f80de318f2182
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2023-11-22 16:37:37 +0200

    Osa 4 apufunktioita ja yksikkötestejä 2

--- osa4/apufunktioita_ja_yksikkotesteja/tests/bloglist.test.js
+++ osa4/apufunktioita_ja_yksikkotesteja/tests/bloglist.test.js
@@ -6,3 +6,50 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Testing 1',
+        author: 'Mr Tester',
+        url: 'test_url',
+        likes: 5,
+        __v: 0
+      }
+    ]
+    const listWithMoreBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Testing 1',
+          author: 'Mr Tester',
+          url: 'test_url',
+          likes: 5,
+          __v: 0
+        },
+        {
+            _id: '558b3292f3572652a58525c7',
+            title: 'Something is out there',
+            author: 'Fear',
+            url: 'testtesttest',
+            likes: 6,
+            __v: 0
+        },
+    ]
+
+    test('of empty list is zero', () => {
+        blogs = []
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(0)
+      })
+
+    test('when list has only one blog equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithMoreBlogs)
+        expect(result).toBe(11)
+      })
+  })

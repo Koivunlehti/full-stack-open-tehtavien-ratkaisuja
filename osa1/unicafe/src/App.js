55b6a0d673548dbf789abbfa0d6e5607a7b65ba2
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2023-11-22 13:11:24 +0200

    Osa 1 unicafe 3

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,17 +6,9 @@ const Button = (props) => (
   </button>
 )
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  return (
+const Statistics = ({good, neutral, bad}) => {
+  return(
     <div>
-      <h2>give feedback</h2>
-      <Button text="good" handleClick={() => setGood(good + 1)}></Button>
-      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)}></Button>
-      <Button text="bad" handleClick={() => setBad(bad + 1)}></Button>
       <h2>statistics</h2>
       good {good}
       <br />
@@ -29,6 +21,22 @@ const App = () => {
       average {(good - bad) / (good + neutral + bad)}
       <br />
       positive {good * 100 / (neutral + bad + good)} %
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button text="good" handleClick={() => setGood(good + 1)}></Button>
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)}></Button>
+      <Button text="bad" handleClick={() => setBad(bad + 1)}></Button>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }

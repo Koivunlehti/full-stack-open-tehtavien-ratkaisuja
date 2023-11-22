53427c25371c80eb3f33fc1aa24b0406ed50f374
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2023-11-22 13:08:25 +0200

    Osa 1 unicafe 1

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button text="good" handleClick={() => setGood(good + 1)}></Button>
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)}></Button>
+      <Button text="bad" handleClick={() => setBad(bad + 1)}></Button>
+      <h2>statistics</h2>
+      good {good}
+      <br />
+      neutral {neutral}
+      <br />
+      bad {bad}
+    </div>
+  )
+}
+
+export default App

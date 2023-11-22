602e5b2f77f6a7b5a6c32037666d9634cb66f087
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2023-11-22 15:22:40 +0200

    Osa 3 puhelinluettelo ja tietokanta 8

--- osa3/puhelinluettelo_ja_tietokanta/backend/models/person.js
+++ osa3/puhelinluettelo_ja_tietokanta/backend/models/person.js
@@ -18,7 +18,16 @@ const personSchema = new mongoose.Schema({
     type: String,    
     minlength: 3
   },
-  number: String,
+  number: {
+    type:String,
+    minlength: 8,
+    validate: {
+      validator: function(val) {
+        return /^\d{2,3}-\d{7,8}$/.test(val);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+  }
 })
 
 personSchema.set('toJSON', {
